59fda8dd475fe1d2520e53ed78399a7f5b4dcda7
Alex A. Naanou <alex.nanou@gmail.com>
2012-08-29 18:50:24 +0400

    migrated the loader to the new format...
    Signed-off-by: Alex A. Naanou <alex.nanou@gmail.com>

--- ui/images.js
+++ ui/images.js
@@ -1,151 +1,448 @@
 var image_list = {
-	"images":[
-		"images/350px/DSC_3501.jpg",
-		"images/350px/DSC_3503.jpg",
-		"images/350px/DSC_3504.jpg",
-		"images/350px/DSC_3506.jpg",
-		"images/350px/DSC_3508.jpg",
-		"images/350px/DSC_3509.jpg",
-		"images/350px/DSC_3511.jpg",
-		"images/350px/DSC_3512.jpg",
-		"images/350px/DSC_3516.jpg",
-		"images/350px/DSC_3517.jpg",
-		"images/350px/DSC_3518.jpg",
-		"images/350px/DSC_3519.jpg",
-		"images/350px/DSC_3522.jpg",
-		"images/350px/DSC_3524.jpg",
-		"images/350px/DSC_3525.jpg",
-		"images/350px/DSC_3528.jpg",
-		"images/350px/DSC_3530.jpg",
-		"images/350px/DSC_3532.jpg",
-		"images/350px/DSC_3534.jpg",
-		"images/350px/DSC_3535.jpg",
-		"images/350px/DSC_3537.jpg",
-		"images/350px/DSC_3542.jpg",
-		"images/350px/DSC_3543.jpg",
-		"images/350px/DSC_3545.jpg",
-		"images/350px/DSC_3546.jpg",
-		"images/350px/DSC_3547.jpg",
-		"images/350px/DSC_3548.jpg",
-		"images/350px/DSC_3549.jpg",
-		"images/350px/DSC_3552.jpg",
-		"images/350px/DSC_3553.jpg",
-		"images/350px/DSC_3555.jpg",
-		"images/350px/DSC_3558.jpg",
-		"images/350px/DSC_3559.jpg",
-		"images/350px/DSC_3560.jpg",
-		"images/350px/DSC_3561.jpg",
-		"images/350px/DSC_3562.jpg",
-		"images/350px/DSC_3565.jpg",
-		"images/350px/DSC_3566.jpg",
-		"images/350px/DSC_3568.jpg",
-		"images/350px/DSC_3569.jpg",
-		"images/350px/DSC_3570.jpg",
-		"images/350px/DSC_3571.jpg",
-		"images/350px/DSC_3572.jpg",
-		"images/350px/DSC_3573.jpg",
-		"images/350px/DSC_3574.jpg",
-		"images/350px/DSC_3575.jpg",
-		"images/350px/DSC_3576.jpg",
-		"images/350px/DSC_3578.jpg",
-		"images/350px/DSC_3581.jpg",
-		"images/350px/DSC_3586.jpg",
-		"images/350px/DSC_3588.jpg",
-		"images/350px/DSC_3590.jpg",
-		"images/350px/DSC_3592.jpg",
-		"images/350px/DSC_3593.jpg",
-		"images/350px/DSC_3594.jpg",
-		"images/350px/DSC_3595.jpg",
-		"images/350px/DSC_3598.jpg",
-		"images/350px/DSC_3599.jpg",
-		"images/350px/DSC_3601.jpg",
-		"images/350px/DSC_3602.jpg",
-		"images/350px/DSC_3605.jpg",
-		"images/350px/DSC_3606.jpg",
-		"images/350px/DSC_3607.jpg",
-		"images/350px/DSC_3608.jpg",
-		"images/350px/DSC_3611.jpg",
-		"images/350px/DSC_3613.jpg",
-		"images/350px/DSC_3614.jpg",
-		"images/350px/DSC_3615.jpg",
-		"images/350px/DSC_3619.jpg",
-		"images/350px/DSC_3623.jpg",
-		"images/350px/DSC_3624.jpg",
-		"images/350px/DSC_3626.jpg",
-		"images/350px/DSC_3627.jpg",
-		"images/350px/DSC_3628.jpg",
-		"images/350px/DSC_3631.jpg",
-		"images/350px/DSC_3632.jpg",
-		"images/350px/DSC_3633.jpg",
-		"images/350px/DSC_3637.jpg",
-		"images/350px/DSC_3638.jpg",
-		"images/350px/DSC_3640.jpg",
-		"images/350px/DSC_3641.jpg",
-		"images/350px/DSC_3643.jpg",
-		"images/350px/DSC_3645.jpg",
-		"images/350px/DSC_3648.jpg",
-		"images/350px/DSC_3650.jpg",
-		"images/350px/DSC_3652.jpg",
-		"images/350px/DSC_3653.jpg",
-		"images/350px/DSC_3656.jpg",
-		"images/350px/DSC_3659.jpg",
-		"images/350px/DSC_3660.jpg",
-		"images/350px/DSC_3661.jpg",
-		"images/350px/DSC_3662.jpg",
-		"images/350px/DSC_3664.jpg",
-		"images/350px/DSC_3666.jpg",
-		"images/350px/DSC_3670.jpg",
-		"images/350px/DSC_3673.jpg",
-		"images/350px/DSC_3675.jpg",
-		"images/350px/DSC_3677.jpg",
-		"images/350px/DSC_3682.jpg",
-		"images/350px/DSC_3685.jpg",
-		"images/350px/DSC_3686.jpg",
-		"images/350px/DSC_3693.jpg",
-		"images/350px/DSC_3694.jpg",
-		"images/350px/DSC_3697.jpg",
-		"images/350px/DSC_3698.jpg",
-		"images/350px/DSC_3699.jpg",
-		"images/350px/DSC_3701.jpg",
-		"images/350px/DSC_3703.jpg",
-		"images/350px/DSC_3705.jpg",
-		"images/350px/DSC_3708.jpg",
-		"images/350px/DSC_3710.jpg",
-		"images/350px/DSC_3711.jpg",
-		"images/350px/DSC_3712.jpg",
-		"images/350px/DSC_3713.jpg",
-		"images/350px/DSC_3714.jpg",
-		"images/350px/DSC_3716.jpg",
-		"images/350px/DSC_3722.jpg",
-		"images/350px/DSC_3723.jpg",
-		"images/350px/DSC_3726.jpg",
-		"images/350px/DSC_3727.jpg",
-		"images/350px/DSC_3730.jpg",
-		"images/350px/DSC_3731.jpg",
-		"images/350px/DSC_3733.jpg",
-		"images/350px/DSC_3736.jpg",
-		"images/350px/DSC_3737.jpg",
-		"images/350px/DSC_3741.jpg",
-		"images/350px/DSC_3744.jpg",
-		"images/350px/DSC_3751.jpg",
-		"images/350px/DSC_3752.jpg",
-		"images/350px/DSC_3753.jpg",
-		"images/350px/DSC_3755.jpg",
-		"images/350px/DSC_3756.jpg",
-		"images/350px/DSC_3757.jpg",
-		"images/350px/DSC_3763.jpg",
-		"images/350px/DSC_3766.jpg",
-		"images/350px/DSC_3781.jpg",
-		"images/350px/DSC_3783.jpg",
-		"images/350px/DSC_3787.jpg",
-		"images/350px/DSC_3790.jpg",
-		"images/350px/DSC_3793.jpg",
-		"images/350px/DSC_3794.jpg",
-		"images/350px/DSC_3801.jpg",
-		"images/350px/DSC_3807.jpg",
-		"images/350px/DSC_3812.jpg",
-		"images/350px/DSC_3813.jpg",
-		"images/350px/DSC_3815.jpg",
-		"images/350px/DSC_3816.jpg",
-	]
+    "position": "0", 
+    "ribbons": [
+        {
+            "133": {
+                "url": "./images/350px/DSC_3763.jpg"
+            }, 
+            "132": {
+                "url": "./images/350px/DSC_3757.jpg"
+            }, 
+            "131": {
+                "url": "./images/350px/DSC_3756.jpg"
+            }, 
+            "130": {
+                "url": "./images/350px/DSC_3755.jpg"
+            }, 
+            "137": {
+                "url": "./images/350px/DSC_3787.jpg"
+            }, 
+            "136": {
+                "url": "./images/350px/DSC_3783.jpg"
+            }, 
+            "135": {
+                "url": "./images/350px/DSC_3781.jpg"
+            }, 
+            "134": {
+                "url": "./images/350px/DSC_3766.jpg"
+            }, 
+            "139": {
+                "url": "./images/350px/DSC_3793.jpg"
+            }, 
+            "138": {
+                "url": "./images/350px/DSC_3790.jpg"
+            }, 
+            "24": {
+                "url": "./images/350px/DSC_3546.jpg"
+            }, 
+            "25": {
+                "url": "./images/350px/DSC_3547.jpg"
+            }, 
+            "26": {
+                "url": "./images/350px/DSC_3548.jpg"
+            }, 
+            "27": {
+                "url": "./images/350px/DSC_3549.jpg"
+            }, 
+            "20": {
+                "url": "./images/350px/DSC_3537.jpg"
+            }, 
+            "21": {
+                "url": "./images/350px/DSC_3542.jpg"
+            }, 
+            "22": {
+                "url": "./images/350px/DSC_3543.jpg"
+            }, 
+            "23": {
+                "url": "./images/350px/DSC_3545.jpg"
+            }, 
+            "28": {
+                "url": "./images/350px/DSC_3552.jpg"
+            }, 
+            "29": {
+                "url": "./images/350px/DSC_3553.jpg"
+            }, 
+            "0": {
+                "url": "./images/350px/DSC_3501.jpg"
+            }, 
+            "4": {
+                "url": "./images/350px/DSC_3508.jpg"
+            }, 
+            "8": {
+                "url": "./images/350px/DSC_3516.jpg"
+            }, 
+            "119": {
+                "url": "./images/350px/DSC_3727.jpg"
+            }, 
+            "120": {
+                "url": "./images/350px/DSC_3730.jpg"
+            }, 
+            "121": {
+                "url": "./images/350px/DSC_3731.jpg"
+            }, 
+            "122": {
+                "url": "./images/350px/DSC_3733.jpg"
+            }, 
+            "123": {
+                "url": "./images/350px/DSC_3736.jpg"
+            }, 
+            "124": {
+                "url": "./images/350px/DSC_3737.jpg"
+            }, 
+            "125": {
+                "url": "./images/350px/DSC_3741.jpg"
+            }, 
+            "126": {
+                "url": "./images/350px/DSC_3744.jpg"
+            }, 
+            "127": {
+                "url": "./images/350px/DSC_3751.jpg"
+            }, 
+            "128": {
+                "url": "./images/350px/DSC_3752.jpg"
+            }, 
+            "129": {
+                "url": "./images/350px/DSC_3753.jpg"
+            }, 
+            "118": {
+                "url": "./images/350px/DSC_3726.jpg"
+            }, 
+            "59": {
+                "url": "./images/350px/DSC_3602.jpg"
+            }, 
+            "58": {
+                "url": "./images/350px/DSC_3601.jpg"
+            }, 
+            "55": {
+                "url": "./images/350px/DSC_3595.jpg"
+            }, 
+            "54": {
+                "url": "./images/350px/DSC_3594.jpg"
+            }, 
+            "57": {
+                "url": "./images/350px/DSC_3599.jpg"
+            }, 
+            "56": {
+                "url": "./images/350px/DSC_3598.jpg"
+            }, 
+            "51": {
+                "url": "./images/350px/DSC_3590.jpg"
+            }, 
+            "50": {
+                "url": "./images/350px/DSC_3588.jpg"
+            }, 
+            "53": {
+                "url": "./images/350px/DSC_3593.jpg"
+            }, 
+            "52": {
+                "url": "./images/350px/DSC_3592.jpg"
+            }, 
+            "115": {
+                "url": "./images/350px/DSC_3716.jpg"
+            }, 
+            "114": {
+                "url": "./images/350px/DSC_3714.jpg"
+            }, 
+            "88": {
+                "url": "./images/350px/DSC_3659.jpg"
+            }, 
+            "89": {
+                "url": "./images/350px/DSC_3660.jpg"
+            }, 
+            "111": {
+                "url": "./images/350px/DSC_3711.jpg"
+            }, 
+            "110": {
+                "url": "./images/350px/DSC_3710.jpg"
+            }, 
+            "113": {
+                "url": "./images/350px/DSC_3713.jpg"
+            }, 
+            "112": {
+                "url": "./images/350px/DSC_3712.jpg"
+            }, 
+            "82": {
+                "url": "./images/350px/DSC_3645.jpg"
+            }, 
+            "83": {
+                "url": "./images/350px/DSC_3648.jpg"
+            }, 
+            "80": {
+                "url": "./images/350px/DSC_3641.jpg"
+            }, 
+            "81": {
+                "url": "./images/350px/DSC_3643.jpg"
+            }, 
+            "86": {
+                "url": "./images/350px/DSC_3653.jpg"
+            }, 
+            "87": {
+                "url": "./images/350px/DSC_3656.jpg"
+            }, 
+            "84": {
+                "url": "./images/350px/DSC_3650.jpg"
+            }, 
+            "85": {
+                "url": "./images/350px/DSC_3652.jpg"
+            }, 
+            "3": {
+                "url": "./images/350px/DSC_3506.jpg"
+            }, 
+            "7": {
+                "url": "./images/350px/DSC_3512.jpg"
+            }, 
+            "108": {
+                "url": "./images/350px/DSC_3705.jpg"
+            }, 
+            "109": {
+                "url": "./images/350px/DSC_3708.jpg"
+            }, 
+            "102": {
+                "url": "./images/350px/DSC_3694.jpg"
+            }, 
+            "103": {
+                "url": "./images/350px/DSC_3697.jpg"
+            }, 
+            "100": {
+                "url": "./images/350px/DSC_3686.jpg"
+            }, 
+            "101": {
+                "url": "./images/350px/DSC_3693.jpg"
+            }, 
+            "106": {
+                "url": "./images/350px/DSC_3701.jpg"
+            }, 
+            "107": {
+                "url": "./images/350px/DSC_3703.jpg"
+            }, 
+            "104": {
+                "url": "./images/350px/DSC_3698.jpg"
+            }, 
+            "105": {
+                "url": "./images/350px/DSC_3699.jpg"
+            }, 
+            "39": {
+                "url": "./images/350px/DSC_3569.jpg"
+            }, 
+            "38": {
+                "url": "./images/350px/DSC_3568.jpg"
+            }, 
+            "33": {
+                "url": "./images/350px/DSC_3560.jpg"
+            }, 
+            "32": {
+                "url": "./images/350px/DSC_3559.jpg"
+            }, 
+            "31": {
+                "url": "./images/350px/DSC_3558.jpg"
+            }, 
+            "30": {
+                "url": "./images/350px/DSC_3555.jpg"
+            }, 
+            "37": {
+                "url": "./images/350px/DSC_3566.jpg"
+            }, 
+            "36": {
+                "url": "./images/350px/DSC_3565.jpg"
+            }, 
+            "35": {
+                "url": "./images/350px/DSC_3562.jpg"
+            }, 
+            "34": {
+                "url": "./images/350px/DSC_3561.jpg"
+            }, 
+            "60": {
+                "url": "./images/350px/DSC_3605.jpg"
+            }, 
+            "61": {
+                "url": "./images/350px/DSC_3606.jpg"
+            }, 
+            "62": {
+                "url": "./images/350px/DSC_3607.jpg"
+            }, 
+            "63": {
+                "url": "./images/350px/DSC_3608.jpg"
+            }, 
+            "64": {
+                "url": "./images/350px/DSC_3611.jpg"
+            }, 
+            "65": {
+                "url": "./images/350px/DSC_3613.jpg"
+            }, 
+            "66": {
+                "url": "./images/350px/DSC_3614.jpg"
+            }, 
+            "67": {
+                "url": "./images/350px/DSC_3615.jpg"
+            }, 
+            "68": {
+                "url": "./images/350px/DSC_3619.jpg"
+            }, 
+            "69": {
+                "url": "./images/350px/DSC_3623.jpg"
+            }, 
+            "2": {
+                "url": "./images/350px/DSC_3504.jpg"
+            }, 
+            "6": {
+                "url": "./images/350px/DSC_3511.jpg"
+            }, 
+            "99": {
+                "url": "./images/350px/DSC_3685.jpg"
+            }, 
+            "98": {
+                "url": "./images/350px/DSC_3682.jpg"
+            }, 
+            "91": {
+                "url": "./images/350px/DSC_3662.jpg"
+            }, 
+            "90": {
+                "url": "./images/350px/DSC_3661.jpg"
+            }, 
+            "93": {
+                "url": "./images/350px/DSC_3666.jpg"
+            }, 
+            "92": {
+                "url": "./images/350px/DSC_3664.jpg"
+            }, 
+            "95": {
+                "url": "./images/350px/DSC_3673.jpg"
+            }, 
+            "94": {
+                "url": "./images/350px/DSC_3670.jpg"
+            }, 
+            "97": {
+                "url": "./images/350px/DSC_3677.jpg"
+            }, 
+            "96": {
+                "url": "./images/350px/DSC_3675.jpg"
+            }, 
+            "11": {
+                "url": "./images/350px/DSC_3519.jpg"
+            }, 
+            "10": {
+                "url": "./images/350px/DSC_3518.jpg"
+            }, 
+            "13": {
+                "url": "./images/350px/DSC_3524.jpg"
+            }, 
+            "12": {
+                "url": "./images/350px/DSC_3522.jpg"
+            }, 
+            "15": {
+                "url": "./images/350px/DSC_3528.jpg"
+            }, 
+            "14": {
+                "url": "./images/350px/DSC_3525.jpg"
+            }, 
+            "17": {
+                "url": "./images/350px/DSC_3532.jpg"
+            }, 
+            "16": {
+                "url": "./images/350px/DSC_3530.jpg"
+            }, 
+            "19": {
+                "url": "./images/350px/DSC_3535.jpg"
+            }, 
+            "18": {
+                "url": "./images/350px/DSC_3534.jpg"
+            }, 
+            "117": {
+                "url": "./images/350px/DSC_3723.jpg"
+            }, 
+            "116": {
+                "url": "./images/350px/DSC_3722.jpg"
+            }, 
+            "48": {
+                "url": "./images/350px/DSC_3581.jpg"
+            }, 
+            "49": {
+                "url": "./images/350px/DSC_3586.jpg"
+            }, 
+            "46": {
+                "url": "./images/350px/DSC_3576.jpg"
+            }, 
+            "47": {
+                "url": "./images/350px/DSC_3578.jpg"
+            }, 
+            "44": {
+                "url": "./images/350px/DSC_3574.jpg"
+            }, 
+            "45": {
+                "url": "./images/350px/DSC_3575.jpg"
+            }, 
+            "42": {
+                "url": "./images/350px/DSC_3572.jpg"
+            }, 
+            "43": {
+                "url": "./images/350px/DSC_3573.jpg"
+            }, 
+            "40": {
+                "url": "./images/350px/DSC_3570.jpg"
+            }, 
+            "41": {
+                "url": "./images/350px/DSC_3571.jpg"
+            }, 
+            "1": {
+                "url": "./images/350px/DSC_3503.jpg"
+            }, 
+            "5": {
+                "url": "./images/350px/DSC_3509.jpg"
+            }, 
+            "9": {
+                "url": "./images/350px/DSC_3517.jpg"
+            }, 
+            "146": {
+                "url": "./images/350px/DSC_3816.jpg"
+            }, 
+            "144": {
+                "url": "./images/350px/DSC_3813.jpg"
+            }, 
+            "145": {
+                "url": "./images/350px/DSC_3815.jpg"
+            }, 
+            "142": {
+                "url": "./images/350px/DSC_3807.jpg"
+            }, 
+            "143": {
+                "url": "./images/350px/DSC_3812.jpg"
+            }, 
+            "140": {
+                "url": "./images/350px/DSC_3794.jpg"
+            }, 
+            "141": {
+                "url": "./images/350px/DSC_3801.jpg"
+            }, 
+            "77": {
+                "url": "./images/350px/DSC_3637.jpg"
+            }, 
+            "76": {
+                "url": "./images/350px/DSC_3633.jpg"
+            }, 
+            "75": {
+                "url": "./images/350px/DSC_3632.jpg"
+            }, 
+            "74": {
+                "url": "./images/350px/DSC_3631.jpg"
+            }, 
+            "73": {
+                "url": "./images/350px/DSC_3628.jpg"
+            }, 
+            "72": {
+                "url": "./images/350px/DSC_3627.jpg"
+            }, 
+            "71": {
+                "url": "./images/350px/DSC_3626.jpg"
+            }, 
+            "70": {
+                "url": "./images/350px/DSC_3624.jpg"
+            }, 
+            "79": {
+                "url": "./images/350px/DSC_3640.jpg"
+            }, 
+            "78": {
+                "url": "./images/350px/DSC_3638.jpg"
+            }
+        }
+    ]
 }
