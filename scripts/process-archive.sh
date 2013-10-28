28f48e7105b55afa95e0d9c6a8f501c34f53fd3c
Alex A. Naanou <alex.nanou@gmail.com>
2013-10-29 02:51:05 +0400

    minor tweaks and fixes...
    Signed-off-by: Alex A. Naanou <alex.nanou@gmail.com>

--- scripts/process-archive.sh
+++ scripts/process-archive.sh
@@ -207,19 +207,19 @@ fi
 
 
 
+# build cache...
+if [ -z $SKIP_CACHE ] ; then
+	find . -type d -name 'preview (RAW)' -exec buildcache "{}" \;
+fi
+
+
+
 # collect previews to one location...
 if ! [ -z $COMMON_PREVIEWS ] ; then
 	if ! [ -e "./$COMMON_PREVIEWS" ] ; then
 		mkdir -p "./$COMMON_PREVIEWS"
 	fi
 	find . -type d -name 'preview (RAW)' -exec mv "{}" "./$COMMON_PREVIEWS" \;
-fi
-
-
-
-# build cache...
-if [ -z $SKIP_CACHE ] ; then
-	find . -type d -name 'preview (RAW)' -exec buildcache "{}" \;
 fi
 
 
