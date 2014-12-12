5698b5b959a095fdda3634fec1e3039e2c441bdc
Alex A. Naanou <alex.nanou@gmail.com>
2014-12-12 03:56:58 +0300

    more ideas, still thinking...
    Signed-off-by: Alex A. Naanou <alex.nanou@gmail.com>

--- ui (gen4)/file.js
+++ ui (gen4)/file.js
@@ -34,6 +34,19 @@ var INDEX_DIR = '.ImageGrid'
 // 	- join indexes
 // 		- take care of different base paths in images
 //
+// Might also be a nice idea to generic import:
+//	- get all .ImageGrid/*.json
+//	- group by ([a-z]*).* — pattern with keyword
+//	- sort by name, descending
+//	- split at first non-diff
+//	- merge diff's in reverse tail to head
+// 
+// And output to format:
+// 	{
+// 		<keyword>: <data>,
+// 		...
+// 	}
+//
 
 function listIndexes(base){
 	return glob(base +'/**/'+ INDEX_DIR)
