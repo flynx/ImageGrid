574f4ff48e8d2bbe9e374ce2f7934591d9eba684
Alex A. Naanou <alex.nanou@gmail.com>
2015-12-31 10:40:25 +0300

    cleanup...
    Signed-off-by: Alex A. Naanou <alex.nanou@gmail.com>

--- ui (gen4)/ig.js
+++ ui (gen4)/ig.js
@@ -15,10 +15,11 @@ requirejs.config({
 	// XXX this does not work on direct filesystem access...
 	//urlArgs: 'bust='+Date.now(),
 })
-
 var _require = require
 require = requirejs
 
+
+//---------------------------------------------------------------------
 
 var core = require('features/core')
 var base = require('features/base')
@@ -34,7 +35,7 @@ var meta = require('features/meta')
 
 
 
-//---------------------------------------------------------------------
+/*********************************************************************/
 
 // NOTE: this is here to simplify importing...
 var ImageGridFeatures =
@@ -43,44 +44,15 @@ module.ImageGridFeatures =
 
 
 
-
-/*********************************************************************/
+//---------------------------------------------------------------------
 
 // XXX stub action set -- this needs to be auto-generated...
 var a = actions.Actions()
 
-// used switch experimental actions on (set to true) or off (unset or false)...
-//a.experimental = true
-
-//a.logger = a.logger || {emit: function(e, v){ console.log('    ', e, v) }}
-
 // setup actions...
-// XXX this will fail because we did not load ui...
 ImageGridFeatures
-	.setup(a, [
-		'viewer-commandline',
-
-		// XXX this is not for production...
-		'experiments',
-	])
+	.setup(a, ['viewer-commandline'])
 	.start()
-
-
-/*
-// report stuff...
-// XXX we also have .conflicts and .missing
-a.features.excluded.length > 0 
-	&& console.warn('Features excluded (%d):',
-		a.features.excluded.length, 
-		a.features.excluded)
-console.log('Features not applicable (%d):', 
-	a.features.unapplicable.length, 
-	a.features.unapplicable)
-console.log('Features loaded (%d):',
-	a.features.features.length, 
-	a.features.features)
-
-*/
 
 
 
