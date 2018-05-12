421c4e96aa50fd056ba950330c72428292915b4d
Alex A. Naanou <alex.nanou@gmail.com>
2018-05-12 13:47:59 +0300

    added ntfs compression support, still not sure about defaults...
    Signed-off-by: Alex A. Naanou <alex.nanou@gmail.com>

--- scripts/compress-archive.sh
+++ scripts/compress-archive.sh
@@ -20,11 +20,22 @@ printhelp(){
 	echo "Arguments:"
 	echo "	-h --help	- print this help and exit."
 	echo
-	echo "	-bzip2		- use bzip2 to compress (default)."
-	echo "	-gzip		- use gzip to compress."
+	echo "	-bz -bzip2	- use bzip2 to compress (default)."
+	echo "	-gz -gzip	- use gzip to compress."
+	echo "	-c -compact	- use ntfs compression."
 	echo
 	echo "	-ext EXT	- set file extension to compress (default: ARW)"
 	echo "			  NOTE: only one -ext is supported now".
+	echo
+	echo "NOTE: not yet sure if to use ntfs compression or bzip2 as default"
+	echo "	they both have advantages and disadvantages:"
+	echo "		ntfs compression:"
+	echo "		  + transparent to all apps -- no extra steps needed"
+	echo "		  - might complicate low level data recovery"
+	echo "		  - transfers may not be transparent -- actual size vs. disk size"
+	echo "		bzip2/gzip/...:"
+	echo "		  + transparent to file operations and recovery"
+	echo "		  - requires manual decompression"
 	echo
 }
 
@@ -37,12 +48,17 @@ while true ; do
 			;;
 
 		# archivers...
-		-bzip2)
+		-bz|--bzip2)
 			ARCH=bzip2 -v \{}
 			shift
 			;;
-		-gzip)
+		-gz|--gzip)
 			ARCH=gzip -v \{}
+			shift
+			;;
+		-c|--compact)
+			# XXX should we cygpath -w all the inputs???
+			ARCH='compact /c /exe:lzx {}'
 			shift
 			;;
 
