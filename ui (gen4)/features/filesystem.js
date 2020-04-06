e86a471e630231b477486ac9147885584a96e4db
Alex A. Naanou <alex.nanou@gmail.com>
2020-04-06 23:53:57 +0300

    todo...
    Signed-off-by: Alex A. Naanou <alex.nanou@gmail.com>

--- ui (gen4)/features/filesystem.js
+++ ui (gen4)/features/filesystem.js
@@ -1762,6 +1762,7 @@ var FileSystemWriterActions = actions.Actions({
 	//					console.log('done.') })
 	// XXX when no previews present this should create at least one file
 	// 		of max_size...
+	// XXX log gid count instead of file count...
 	exportIndex: ['- File/Export/Export index',
 		function(path, max_size, include_orig, clean_target_dir, logger){
 			var that = this
