9fe6678b9521ad33b09ad16feada02b052acddb3
Alex A. Naanou <alex.nanou@gmail.com>
2025-02-01 20:23:52 +0300

    refactoring and cleanup...
    Signed-off-by: Alex A. Naanou <alex.nanou@gmail.com>

--- Archive/snapshot.sh
+++ Archive/snapshot.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/bash
 
-# XXX this should run in script dir (not cwd)...
-
 # XXX need:
 # 	- protocol (command) to create archive root
+# 		manually:
+# 			- copy tree
+# 			- run snapshot.sh
+# 		script:
+# 			XXX
 # 	- protocol to create snapshots
 # 		- sync-flash.sh ???
 # 	- protocol to restore stuff -- simply copy???
@@ -13,6 +16,16 @@
 # 	- a way to restore specicifc file(s)
 # 	- a way to maintain a set number of snapshots...
 #
+
+SNAPSHOT_DIR=.snapshots
+SUBVOLUME_DIR=media
+
+
+# run in script dir (not cwd)...
+DIR=`dirname "$0"`
+if ! [ -z "$DIR" ] ; then
+	cd "$DIR"
+fi
 
 # check if on btrfs filesystem...
 # XXX also check if btrfs command is available...
@@ -26,29 +39,32 @@ fi
 
 # create ./media...
 # XXX check if not a directory...
-if ! [ -e ./media ] ; then
-	btrfs subvolume create ./media
+if ! [ -e "$SUBVOLUME_DIR" ] ; then
+	btrfs subvolume create "$SUBVOLUME_DIR"
 	# XXX build tree -- ImageGrid
 
 # convert ./media to a subvolume...
-elif [ "$(stat --format=%i ./media)" == 256 ] ; then
+elif [ "$(stat --format=%i "$SUBVOLUME_DIR")" == 256 ] ; then
 	mkdir bak
-	mv media bak/
-	btrfs subvolume create ./media
+	mv "$SUBVOLUME_DIR" bak/
+	btrfs subvolume create "$SUBVOLUME_DIR"
 	cp --archive --one-file-system --reflink=always \
-		./bak/media/{,.}* \
-		./media/
+		./bak/"$SUBVOLUME_DIR"/{,.}* \
+		"$SUBVOLUME_DIR"/
 fi
-mkdir -p ./.snapshots
+mkdir -p "$SNAPSHOT_DIR"
 
 
 # XXX should this be more human readable???
 # 	...a date + number maybe???
 SNAPSHOT=$(( 
-	$( ls .snapshots \
+	$( ls "$SNAPSHOT_DIR" \
 		| sort -n \
 		| tail -n 1 ) \
 	+ 1 ))
 
-btrfs subvolume snapshot -r ./media .snapshots/${SNAPSHOT}
+btrfs subvolume snapshot -r "$SUBVOLUME_DIR" "${SNAPSHOT_DIR}/${SNAPSHOT}"
 
+
+
+# vim:set nowrap nospell :
